5b8247247a5ef4db85cdc5d8d6be3edbe4b38ce0
Taylor Hirth <taylorhirth19@gmail.com>
2025-03-14 15:00:56 -0500

    added script into App.js, created data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+console.log("puppyList: ", puppyList);
 
   return (
-    <>
-
-    </>
-  )
-}
+    <div className="App">
+  {
+    puppies.map((puppy) => {
+      return <p key={puppy.id}>{puppy.name}</p>;
+    })
+  }
+    </div>
+  );
+};
 
 export default App
